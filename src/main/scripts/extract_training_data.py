c652e7f0f7246949f0570b45e06e20fddfc5e08d
pushprajsingh <mailpushpraj@gmail.com>
2016-11-07 08:42:53 +0530

    Update extract_training_data.py

--- src/main/scripts/extract_training_data.py
+++ src/main/scripts/extract_training_data.py
@@ -58,15 +58,16 @@ def download_weather_data(location_info_list, start_date, api_key):
                 
                 for hour in forecast.hourly().data:
                    # print(hour)
-                    weather_data['loc'] = weather_data.get('loc', []) + [location_info['location']]
+
+                    weather_data['elev'] = weather_data.get('elev', []) + [location_info['elev']]
+                    weather_data['hum'] = weather_data.get('hum', []) + [hour.d.get('humidity', 0.5)]
                     weather_data['lat'] = weather_data.get('lat', []) + [location_info['lat']]
                     weather_data['lng'] = weather_data.get('lng', []) + [location_info['lng']]
-                    weather_data['elev'] = weather_data.get('elev', []) + [location_info['elev']]
-                    weather_data['cond'] = weather_data.get('cond', []) + [hour.d.get('summary', '')]
-                    weather_data['temp'] = weather_data.get('temp', []) + [hour.d.get('temperature', 50)]
-                    weather_data['hum'] = weather_data.get('hum', []) + [hour.d.get('humidity', 0.5)]
                     weather_data['pres'] = weather_data.get('pres', []) + [hour.d.get('pressure', 1000)]
+                    weather_data['temp'] = weather_data.get('temp', []) + [hour.d.get('temperature', 50)]
                     weather_data['time'] = weather_data.get('time', []) + [hour.d['time']]
+                    weather_data['loc'] = weather_data.get('loc', []) + [location_info['location']]
+                    weather_data['cond'] = weather_data.get('cond', []) + [hour.d.get('summary', '')]
             except:
                 return weather_data
 
