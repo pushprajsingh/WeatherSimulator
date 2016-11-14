3baa08e2c9be668a39f65a0d92ff25271df696ef
pushprajsingh <mailpushpraj@gmail.com>
2016-11-14 11:40:03 +0530

    Update ParseDoubleDatatoLabeledPoint.java

--- src/main/java/com/weather/functions/ParseDoubleDatatoLabeledPoint.java
+++ src/main/java/com/weather/functions/ParseDoubleDatatoLabeledPoint.java
@@ -21,21 +21,23 @@ public class ParseDoubleDatatoLabeledPoint implements Function<String, LabeledPo
 	private static final long serialVersionUID = 1L;
 	private final List<Integer> prediction;
 	private final List<Integer> label;
+	private boolean validAttributes=true;
 	private static final Log LOG = LogFactory.getLog(ParseDoubleDatatoLabeledPoint.class);
 	
 	public ParseDoubleDatatoLabeledPoint(MLibModel mlibModel) {
 		this.prediction=mlibModel.getPrediction();
 		this.label= mlibModel.getLabel();
+		this.validAttributes=checkValidAttributes();
 	}	
 	
 	@Override
 	public LabeledPoint call(String arg0) throws Exception {
 		//LOG.info("Calling main function of ParseDoubleDatatoLabeledPoint..");
-		 if(arg0==null)
+		 if(arg0==null || !validAttributes)
 		 {
 			 return new LabeledPoint(-1, Vectors.zeros(prediction.size()));
 		 }
-		 
+		 //
 		 String[] parts = arg0.split(",");
          double[] v = new double[prediction.size()];
          for(int i=0;i<prediction.size();i++)
@@ -45,7 +47,7 @@ public class ParseDoubleDatatoLabeledPoint implements Function<String, LabeledPo
            	 {
            		 num=Double.parseDouble(parts[prediction.get(i)]);
            	 }
-           	 catch(NumberFormatException e)
+           	 catch(NumberFormatException |ArrayIndexOutOfBoundsException e )
            	 {
            		 return new LabeledPoint(-1, Vectors.zeros(prediction.size()));
            	 }
@@ -54,6 +56,20 @@ public class ParseDoubleDatatoLabeledPoint implements Function<String, LabeledPo
          }
          return new LabeledPoint(Double.parseDouble(parts[label.get(0)]), Vectors.dense(v));     
 		
+	}
+	
+	private boolean checkValidAttributes()
+	{
+		if ( prediction==null || prediction.size()==0 ||
+			 label==null || label.size()==0
+		   )		
+		{
+			return false;
+		}
+		else 
+		{
+			return true;
+		}
 	}
 	
 }
